0328f9544818514d10d105e7df7be81e8b8d377b
İlker Ata Şahin <ilkeratasahin@gmail.com>
2026-06-11 18:24:58 +0300

    chore: remove empty pom name tags and fix status DTO formatting

--- job-application-service/src/main/java/dev/aegeanship/jobtracker/jobapplicationservice/application/dto/request/UpdateApplicationStatusRequest.java
+++ job-application-service/src/main/java/dev/aegeanship/jobtracker/jobapplicationservice/application/dto/request/UpdateApplicationStatusRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotNull;
 public record UpdateApplicationStatusRequest(
 
         @NotNull(message = "Target status is required")
-        ApplicationStatus toStatus ,
+        ApplicationStatus toStatus,
 
         String note
 ) {
--- job-application-service/src/main/java/dev/aegeanship/jobtracker/jobapplicationservice/interview/dto/request/UpdateInterviewStatusRequest.java
+++ job-application-service/src/main/java/dev/aegeanship/jobtracker/jobapplicationservice/interview/dto/request/UpdateInterviewStatusRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotNull;
 public record UpdateInterviewStatusRequest(
 
         @NotNull(message = "Target status is required")
-        InterviewStatus toStatus ,
+        InterviewStatus toStatus,
 
         String note
 ) {
