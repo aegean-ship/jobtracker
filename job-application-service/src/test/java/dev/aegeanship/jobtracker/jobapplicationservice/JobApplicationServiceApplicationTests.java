11c54e67cd934971252d44e01c0b7cf672dafa16
alirizakaygusuz <ali.rizakaygusuz@hotmail.com>
2026-06-11 12:05:09 +0300

    chore: initialize jobtracker monorepo

---
+++ job-application-service/src/test/java/dev/aegeanship/jobtracker/jobapplicationservice/JobApplicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aegeanship.jobtracker.jobapplicationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user-service/src/test/java/dev/aegeanship/jobtracker/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aegeanship.jobtracker.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
